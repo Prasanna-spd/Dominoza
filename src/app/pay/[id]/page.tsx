6240d87754240b24843fb73acf7f4a651238665c
Sarada Prasanna Dash <dashsharada@gmail.com>
2025-05-28 12:19:45 +0530

    modified params in pageprops to use hook react

--- src/app/pay/[id]/page.tsx
+++ src/app/pay/[id]/page.tsx
@@ -3,14 +3,14 @@
 import CheckoutForm from "@/components/CheckoutForm";
 import { Elements } from "@stripe/react-stripe-js";
 import { StripeElementsOptions, loadStripe } from "@stripe/stripe-js";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
-const PayPage = ({ params }: { params: { id: string } }) => {
+const PayPage = ({ params }: { params: Promise<{ id: string }> }) => {
   const [clientSecret, setClientSecret] = useState("");
 
-  const { id } = params;
+  const { id } = use(params);
 
   useEffect(() => {
     const makeRequest = async () => {
