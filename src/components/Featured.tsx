8c1cc5587c67f8843553307b73799cdfd75bde7e
Sarada Prasanna Dash <dashsharada@gmail.com>
2025-05-28 12:29:00 +0530

    modified text color

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -39,7 +39,7 @@ const Featured = async () => {
               <h1 className="text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl">{item.title}</h1>
               <p className="p-4 2xl:p-8">{item.desc}</p>
               <span className="text-xl font-bold">${item.price}</span>
-              <button className="bg-red-500 text-white p-2 rounded-md">
+              <button className="bg-blue-600 text-white p-2 rounded-md">
                 Add to Cart
               </button>
             </div>
